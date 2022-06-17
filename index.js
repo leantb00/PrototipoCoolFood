6fa349d0b59848a0b57900725b764e194a9fcde9
LeandroBraga <leantb00@gmail.com>
2022-06-17 01:16:31 -0300

    changed

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 app.use(express.static('public'));
-const port = 3000
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/public/index.html")
 })
+
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
